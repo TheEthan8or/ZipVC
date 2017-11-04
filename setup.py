e0212b9e78294aa114dbe8a86d0d3edfce764ba1
TSDude <ts-programs@outlook.com>
2017-11-03 21:30:18 -0600

    Added dependencies for application in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ZipVersionControl',
-    version='0',
-    packages=[''],
+    version='0.0.0.4',
+    packages=find_packages(),
     url='https://bitbucket.org/TSDude/zipversioncontrol',
     license='MIT License',
     author='team1790',
     author_email='ts-programs@outlook.com',
-    description='Allows git version control with zip archives'
+    description='Allows git version control with zip archives',
+    install_requires=['colorama', 'gitpython']
 )
